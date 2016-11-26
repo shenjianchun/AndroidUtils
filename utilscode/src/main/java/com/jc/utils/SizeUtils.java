49ec34a7f8d98526430196a7b74192675bfc49fb
shenjianchun <shenjianchun@gmail.com>
2016-11-26 17:35:57 +0800

    初始化项目

---
+++ utilscode/src/main/java/com/jc/utils/SizeUtils.java
@@ -0,0 +1,79 @@
+package com.jc.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+/**
+ * 尺寸相关的工具类
+ * Created by JC on 2016-08-03.
+ */
+public class SizeUtils {
+
+    /* cannot be instantiated */
+    private SizeUtils() {
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    /**
+     * dp 转 px
+     * @param context Context
+     * @param dpValue dp值
+     * @return px值
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px 转 dp
+     * @param context Context
+     * @param pxValue px值
+     * @return dp值
+     */
+    public static int px2dp(Context context, int pxValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5);
+    }
+
+
+    /**
+     * sp转px
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px转sp
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 各种单位转换
+     * 该方法存在于TypedValue
+     */
+    public static float applyDimension(int unit, float value, DisplayMetrics metrics) {
+        switch (unit) {
+            case TypedValue.COMPLEX_UNIT_PX:
+                return value;
+            case TypedValue.COMPLEX_UNIT_DIP:
+                return value * metrics.density;
+            case TypedValue.COMPLEX_UNIT_SP:
+                return value * metrics.scaledDensity;
+            case TypedValue.COMPLEX_UNIT_PT:
+                return value * metrics.xdpi * (1.0f / 72);
+            case TypedValue.COMPLEX_UNIT_IN:
+                return value * metrics.xdpi;
+            case TypedValue.COMPLEX_UNIT_MM:
+                return value * metrics.xdpi * (1.0f / 25.4f);
+        }
+        return 0;
+    }
+
+}
